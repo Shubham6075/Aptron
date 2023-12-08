e2e6cc8922145f427d96d5496501e1349fdec802
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-12-08 13:50:19 +0530

    Add files via upload

---
+++ Core Java/Methods/scopeOfMethods.java
@@ -0,0 +1,11 @@
+package core_java1.Methods;
+
+public class scopeOfMethods {
+	public static void main(String[] args) {
+
+		int x = 30;  // scope of this x is in main method only
+
+		System.out.println(x);
+	}
+
+}
