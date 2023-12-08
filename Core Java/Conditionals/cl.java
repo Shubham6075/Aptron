71bb718756f58d9f45dd093a433b84cfcc4b0542
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-12-08 13:55:28 +0530

    Add files via upload

---
+++ Core Java/Conditionals/cl.java
@@ -0,0 +1,16 @@
+package core_java1.Conditionals;
+
+public class cl {
+	public static void main(String[] args) {
+
+		int a = 30;
+		int b = 20;
+
+		if (a > b) {
+			System.out.println("A is Greater ");
+		} else {
+			System.out.println("A is smaller than B");
+		}
+	}
+
+}
